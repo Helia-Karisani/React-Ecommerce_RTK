8706acabb2c128e027e6518401fa48a50dcb56dd
Helia_Karisani <helia.karisani@gmail.com>
2025-06-19 15:44:17 -0400

    calculating total amount in ShoppingCart.jsx

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,8 +1,17 @@
+// This component displays and managing products in shopping cart
 import React from 'react';
 import './ShoppingCart.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice'; // Assuming you have action creators for increasing and decreasing item quantity
+import './ShoppingCart.css';
 
 const ShoppingCart = () => {
 
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems);
+  // initially, "item" below, is the first item in the cartItems array
+  const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  
   return (
     <>
     <div className="shopping-cart">
