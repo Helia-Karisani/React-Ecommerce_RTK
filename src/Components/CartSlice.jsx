5532cbea6575036ca1780b290dd1e1eb6790bc4e
Helia_Karisani <helia.karisani@gmail.com>
2025-06-19 13:28:32 -0400

    Reducers creation and export actions

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -5,25 +5,37 @@ import { createSlice } from '@reduxjs/toolkit';
 // the information of product quantity entered by you should be available globally to any component.
 const CartSlice = createSlice({
     name: 'cart',
-    initialState,
+    initialState, // The state passed into each reducer (like addItemToCart) represents the current state of the slice. 
     reducers:{
         addItemToCart(state, action) {
-
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if(existingItem){
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({...action.payload, quantity: 1});
+            }
         },
 
         removeItemFromCart(state, action){
-
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
         },
 
         clearCart(state){
-
+            state.cartItems = [];
         },
 
         increaseItemQuantity(state, action){
-
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+      }
         },
 
         decreaseItemQuantity(state, action){
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+      }
 
         },
 
@@ -31,6 +43,14 @@ const CartSlice = createSlice({
     
 });
 
+export const {
+  addItemToCart,
+  removeItemFromCart,
+  clearCart,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
 
 const initialState = {
     cartItems: [],
