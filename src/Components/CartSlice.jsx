f5c50b9fb625f87854af1c8bbca21b86bb920b27
Helia_Karisani <helia.karisani@gmail.com>
2025-06-19 12:35:41 -0400

    Actions and reducers creation

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+//  apply logic for Redux toolkit to ensure that when you 
+// click the Add Product button to add a product to the cart, 
+// the information of product quantity entered by you should be available globally to any component.
+const CartSlice = createSlice({
+    
 });
 
+
+const initialState = {
+    cartItems: [],
+};
 
